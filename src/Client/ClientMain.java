1e5f32273e44f0bd68a77c05315c8dd4dbf64747
Cervo88 <lorycervino2017@gmail.com>
2026-02-16 11:49:57 +0100

    aggiunta comunicazione tra server e client e la chiusura di essa

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,6 +1,9 @@
 package Client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientMain {
@@ -13,10 +16,33 @@ public class ClientMain {
         try {
             Socket socket = new Socket("localhost", 3000);
 
+            //Stream scrittura
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true
+            );
+
+            //Stream lettura
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            out.println("CLIENT: Ciao");
+
+            String invio = in.readLine();
+            System.out.println("ClIENT: risposta = " + invio);
+
+            in.close();
+            out.close();
+            socket.close();
+
+            System.out.println("CLIENT: chiusura della comunicazione");
+
+
+
         } catch (IOException e) {
             System.err.println("Errore nella connesione del server");
 
         }
-    } //ciao
+    }
 
     }
--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,6 +1,9 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,13 +14,38 @@ public class ServerMain {
         try {
             ServerSocket server = new ServerSocket(3000);
             System.out.println("SERVER: in attesa della richiesta del client");
+
             Socket clientSocket = server.accept();
             System.out.println("SERVER: il client si è connesso");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream())
+            );
+
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true
+            );
+
+            //lettura richiesta
+            String richiesta = in.readLine();
+            System.out.println("SERVER: richiesta = " + richiesta);
+
+
+            out.println("Messaggio ricevuto nel server");
+
+            //chiusura
+            in.close();
+            out.close();
+            clientSocket.close();
+            server.close();
+
+            System.out.println("SERVER: chiusura della comunicazione");
 
         } catch (IOException e) {
             System.err.println("porta non disponibile," + "Errore nella creazione del connetion port");
         }
 
+
     }
     }
-//ciao
+
